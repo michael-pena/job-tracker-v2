14889943a31aae3ff54383ad21ae7895d76e886d
michael-pena <michael.pena@protonmail.ch>
2025-02-01 20:25:30 -0600

    initial commit

---
+++ src/main/java/com/mpena/jobtrackerv2/Jobtrackerv2Application.java
@@ -0,0 +1,13 @@
+package com.mpena.jobtrackerv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jobtrackerv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Jobtrackerv2Application.class, args);
+	}
+
+}
